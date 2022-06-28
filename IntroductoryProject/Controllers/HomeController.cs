c5c8413aa966af44d6a01bfa09c98a9cd56ee0d6
Sujon <s.c.shil94@gmail.com>
2022-06-28 23:38:05 +0600

    View model added and passed from controller to view

--- IntroductoryProject/Controllers/HomeController.cs
+++ IntroductoryProject/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace IntroductoryProject.Controllers
 {
@@ -6,7 +7,9 @@ namespace IntroductoryProject.Controllers
     {
         public ViewResult Index()
         {
-            return View("MyView");
+            int hour = DateTime.Now.Hour;
+            string ViewModel = hour < 12 ? "Good morning" : "Good aternoon";
+            return View("MyView",ViewModel);
         }
     }
 }
