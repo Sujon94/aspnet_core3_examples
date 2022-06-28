0b797d9eff32f0b95a29cf3dad7910e17096d18b
Sujon <s.c.shil94@gmail.com>
2022-06-29 00:54:23 +0600

    RSVP form added.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+    }
+}
